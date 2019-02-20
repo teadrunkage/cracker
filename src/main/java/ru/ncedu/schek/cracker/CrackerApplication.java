dba54e91ea2fcbfb185e3c46afed1d61ca5aaba1
Daria Schek <sdasha81@yandex.ru>
2019-02-20 18:30:03 +0300

    First commit

---
+++ src/main/java/ru/ncedu/schek/cracker/CrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.ncedu.schek.cracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ncedu/schek/cracker/CrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.ncedu.schek.cracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
