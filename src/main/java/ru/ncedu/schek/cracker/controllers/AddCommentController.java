a39b933e63e3736bb4f6303e7e078d9a3fc43782
Daria Schek <sdasha81@yandex.ru>
2019-03-11 00:21:00 +0300

    added comments

--- src/main/java/ru/ncedu/schek/cracker/controllers/AddCommentController.java
+++ src/main/java/ru/ncedu/schek/cracker/controllers/AddCommentController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
+import ru.ncedu.schek.cracker.forms.CommentForm;
+
 
 @Controller
 public class AddCommentController {
